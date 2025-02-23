a6f964a8164acbd017d85c9bf2b76301e4322688
chahe-dridi <chaher.dridi.100@gmail.com>
2025-02-23 14:51:22 +0100

    add register for employee

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,62 +1,67 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        axios.post('', { name, email, password })
-            .then((res) => {
-                console.log(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3001/register', { name, email, password })
+      .then((res) => {
+        console.log(res.data);
+      })
+        .then(() => {   
+        navigate('/login');
+        })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h2>Signup</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Signup</button>
-            </form>
-            <Link to="/login">Already have an account? Login</Link>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
         </div>
-    );
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+      <Link to="/login">Already have an account? Login</Link>
+    </div>
+  );
 }
 
 export default Signup;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const EmployeeModel = require('./models/Employee');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/employee', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.post('/register', (req, res) => {
+  EmployeeModel.create(req.body)
+    .then((employee) => res.json(employee))
+    .catch((err) => res.status(400).json(err));
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
